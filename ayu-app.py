a09e51fcbaf2b5391b3373b117a268aabbc2d240
ayusudi <ayusudi.abc@gmail.com>
2023-11-30 15:40:59 +0700

    is done

--- ayu-app.py
+++ ayu-app.py
@@ -1,5 +1,20 @@
 """
-Nama : 
-Batch : 
+Nama : Ayu ....
+Batch : ...
 Objective : Memahami pembuatan REST API dengan FASTAPI 
 """
+
+from fastapi import FastAPI, HTTPException
+import pandas as pd
+
+
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+  try : 
+    df = pd.read_csv("data.csv")
+    return df.to_dict(orient="records")
+  except : 
+    return HTTPException(status_code=500, detail="Internal server error")
