21d58b99782451d4fda194304ded9292c7d029a7
ayusudi <ayusudi.abc@gmail.com>
2023-11-30 14:40:40 +0700

    batch-026

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ Tuliskan step by step pengerjaan agar bisa jadi panduanmu nanti! (optional)
 
 1. Clone repository 
 2. Open bigquery, colab dan visual studio code, colab
-3. 
-4. 
+3. Open catatan
+4. Tulis Intro di setiap file
 5. 
--- ayu-app.py
+++ ayu-app.py
@@ -3,19 +3,3 @@ Nama :
 Batch : 
 Objective : Memahami pembuatan REST API dengan FASTAPI 
 """
-import pandas as pd
-from fastapi import FastAPI, HTTPException
-
-app = FastAPI()
-
-@app.get("/")
-def root():
-  try : 
-    df = pd.read_csv('data.csv')
-    return df.to_dict(orient='records')
-  except : 
-    return HTTPException(status_code=500, detail="Internal server error")
-
-# pip install fastapi
-
-# uvicorn filename:app --reload
